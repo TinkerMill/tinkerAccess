6b43a8ec0ea5c27a63ce7a79907e0222268fc7ec
Matt Poepping <poepping@Matts-Air.main.tinkermill.local>
2016-05-12 18:36:47 -0600

    remove server debug line

--- server.py
+++ server.py
@@ -261,5 +261,5 @@ def viewLog():
   return render_template('admin_log.html', logs=logs)
 
 if __name__ == "__main__":
-  #app.run(host='0.0.0.0')
-  app.run(host='127.0.0.1', debug=True)
+  app.run(host='0.0.0.0')
+  #app.run(host='127.0.0.1', debug=True)
