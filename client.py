f363331cfd74d87f25763338115c8ec1c197b6e2
analog <analog@analogpixel.org>
2016-05-25 20:55:22 -0600

    read 12 prev chars

--- client.py
+++ client.py
@@ -257,8 +257,7 @@ def loop():
 
     # if the serial port has data read it.
     if serialConnection.inWaiting() > 1:
-      #badgeCode = serialConnection.readline().strip()[-13:]
-      badgeCode = serialConnection.readline().strip()[3:]
+      badgeCode = serialConnection.readline().strip()[-12:]
       serialConnection.flushInput()
       serialConnection.flushOutput()  
 
