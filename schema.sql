8a63d05f822ccada144da13edb0de22490a2209b
Todd <todd@katydid.localdomain>
2022-08-02 11:49:58 -0600

    Added a feature where users can be deactivated instead of deleting. Also added timed access to devices and ability to lockout for maintenance. Also added 24hr access option to users to override device timed access.

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
 -- from serv import init_db
 -- init_db()
 
-CREATE TABLE user(id INTEGER PRIMARY KEY, name TEXT, code TEXT);
-CREATE TABLE device(id INTEGER PRIMARY KEY, name TEXT, allUsers BOOLEAN);
+CREATE TABLE user(id INTEGER PRIMARY KEY, name TEXT, code TEXT, status CHARACTER(1) DEFAULT('A'));
+CREATE TABLE device(id INTEGER PRIMARY KEY, name TEXT, allUsers BOOLEAN, lockout INTEGER DEFAULT(0), lockout_start TEXT DEFAULT('12:00 AM'), lockout_end TEXT DEFAULT('12:00 AM'))
 CREATE TABLE deviceAccess(id INTEGER PRIMARY KEY, user INTEGER, device INTEGER, time INTEGER, trainer BOOLEAN);
 create table newuser(id INTEGER PRIMARY KEY, code TEXT, deviceID INTEGER);
 create table log(id INTEGER PRIMARY KEY, message TEXT, Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);
@@ -10,8 +10,8 @@ create table log(id INTEGER PRIMARY KEY, message TEXT, Timestamp DATETIME DEFAUL
 insert into device (id, name, allUsers) values (0, 'laser cutter', 0);
 insert into device (id, name, allUsers) values (1, '3d printer', 0);
 
-insert into user (id, name, code) values (1, 'ron', '150060E726B4');
-insert into user (id, name, code) values (2, 'test', 'a');
+insert into user (id, name, code, status) values (1, 'ron', '150060E726B4', 'A');
+insert into user (id, name, code, status) values (2, 'test', 'a', 'I');
 
 insert into deviceAccess (id, user, device, time) values ( 1, 2, 0, 100);
 
