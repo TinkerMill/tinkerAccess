53b4412ba41be6e6d4702edba8a9b7cd426c8291
David Stearns <David@DavidStearns.us>
2026-02-16 10:54:21 -0700

    First pass at events for WebSocket comms

--- tinker-access-server-rs/src/messages/events.rs
+++ tinker-access-server-rs/src/messages/events.rs
@@ -1,34 +1,14 @@
-use chrono::DateTime;
+use chrono::{DateTime, Utc};
+use serde::Deserialize;
+use ulid::Ulid;
 
+#[derive(Serialize, Deserialize)]
 pub enum Context {
     ToHost,
     FromHost,
 }
 
-pub struct Header {
-    context: Context,
-    ulid: Option<Ulid>,
-}
-
-pub enum CmdMsgType {
-    SetStateCmd,
-}
-
 #[derive(Serialize, Deserialize)]
-#[serde(tag = "trigType")]
-pub enum EventTrigger {
-    IO {
-        io_name: String,         // TODO: Change to Enum?
-        previous_state: Boolean, // was
-        current_state: Boolean,  // is
-    },
-    FreshScan {
-        badge_id: String,
-    },
-    BootUp {},
-    FatalError {},
-}
-
 pub enum BootState {
     Running,
     Unknown,
@@ -37,93 +17,183 @@ pub enum BootState {
     WaitingForConfig,
 }
 
-// TODO: Is this an OK encoding for ESP32's LED settings?
+#[derive(Serialize, Deserialize)]
 pub struct LedState {
     red: Byte,
     green: Byte,
     blue: Byte,
+    remaining: Option<u8>,
 }
 
-type IoName = String;
-pub enum IoSettings {
-    BinarySettings {
-        state: Boolean,
-        next_state: Option<Boolean>,
-        duration: Option<Duration>,
-    },
-    LCDSettings {
-        state: String,
-        next_state: Option<String>,
-        duration: Option<Duration>,
-    },
-    LEDSettings {
-        state: LedState,
-        next_state: Option<LedState>,
-        duration: Option<Duration>,
-    },
+#[derive(Serialize, Deserialize)]
+pub struct OutputState {
+    state: bool,
+    remaining: Option<u8>,
 }
 
-pub struct Config {
-    role: Option<String>,
-    location: Option<String>,
+#[derive(Serialize, Deserialize)]
+pub struct DisplayState {
+    state: String,
+    #[serde(flatten)]
+    next_state: Option<DisplayTransition>,
+}
+#[derive(Serialize, Deserialize)]
+pub struct DisplayTransition {
+    state: String,
+    remaining: u8,
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum IoState {
+    LedState(LedState),
+    OutputState(OutputState),
+    DisplayState(DisplayState),
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct BinaryEventTrigger {
+    trig_type: TriggerType,
+    io_name: String,
+    was: Option<bool>,
+    is: bool,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct DisplayEventTrigger {
+    trig_type: TriggerType,
+    io_name: String,
+    was: Option<String>,
+    is: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct LedEventTrigger {
+    trig_type: TriggerType,
+    io_name: String,
+    was: Option<LedState>,
+    is: LedState,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CardEventTrigger {
+    trig_type: TriggerType,
+    io_name: String,
+    is: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Header {
+    api_version: String,
+    context: Context,
+    ulid: Ulid,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct DeviceConfig {
+    role: String,
+    location: String,
     token: String,
-    init_state: IoSettings,
+    init_state: HashMap<String, IoState>,
 }
 
-type DeviceFeatures = String;
-pub struct DeviceInfoPlatform {
+#[derive(Serialize, Deserialize)]
+pub struct IoOptions {
+    count: Option<u8>,
+    exists: bool,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct PlatformInfo {
     device: String,
     chip_rev: String,
-    mac_addr: String,
-    features: Set<DeviceFeatures>,
+    mac_addr: String, // TODO: Change to a type for mac addresses
+    features: HashMap<String, IoOptions>,
 }
 
-pub struct DeviceInfoApplication {
+#[derive(Serialize, Deserialize)]
+pub struct ApplicationInfo {
     name: String,
     version: String,
-    esp_idf: String, // TODO: Maybe bury this so we aren't locked to ESP in the API?
-    compile_datetime: DateTime<Utc>,
+    framework_version: String,
+    compile_time: DateTime<Utc>,
     elf_sha: String,
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct DeviceInfo {
-    platform: DeviceInfoPlatform,
-    application: DeviceInfoApplication,
+    platform: PlatformInfo,
+    application_info: ApplicationInfo,
 }
 
 #[derive(Serialize, Deserialize)]
 #[serde(tag = "msgType")]
-pub enum Message {
-    CmdResponse {
+pub enum Events {
+    EventReportBinary {
+        #[serde(flatten)]
         header: Header,
-        cmd_msg_type: CmdMsgType,
-        origin_message_ulid: Ulid,
-        succeeded: Boolean,
-        error_message: Option<String>,
+        event_trigger: BinaryEventTrigger,
     },
-    GetStateCmd {
+    EventReportDisp {
+        #[serde(flatten)]
         header: Header,
+        event_trigger: DisplayEventTrigger,
     },
-    EventReport {
+    EventReportLed {
+        #[serde(flatten)]
         header: Header,
-        event_trigger: EventTrigger,
-        boot_state: BootState,
+        event_trigger: LedEventTrigger,
+    },
+    EventReportCard {
+        #[serde(flatten)]
+        header: Header,
+        event_trigger: CardEventTrigger,
+    },
+    EventReportPostBoot {
+        #[serde(flatten)]
+        header: Header,
+        event_trigger: HashMap<String, IoState>,
     },
     SetStateCmd {
+        #[serde(flatten)]
         header: Header,
-        settings: Map<IoName, IoSettings>,
+        settings: HashMap<String, IoState>,
     },
-    GetConfigCmd {
+    BadCmd {
+        #[serde(flatten)]
         header: Header,
-        config: Config,
+        cmd_msg_type: String,
+        cmd_ulid: Ulid,
+        succeeded: bool,
+        error_message: String,
+    },
+    GetStateCmd {
+        #[serde(flatten)]
+        header: Header,
+    },
+    StateReport {
+        #[serde(flatten)]
+        header: Header,
+        boot_state: BootState,
+        current_io_state: HashMap<String, IoState>,
     },
     SetConfigCmd {
+        #[serde(flatten)]
         header: Header,
-        config: Config,
+        config: DeviceConfig,
+    },
+    SetDefaultConfigCmd {
+        #[serde(flatten)]
+        header: Header,
+        config: DeviceConfig,
+    },
+    GetConfigCmd {
+        #[serde(flatten)]
+        header: Header,
     },
     ConfigReport {
+        #[serde(flatten)]
         header: Header,
-        config: Config,
-        device_info: DeviceInfo,
+        config: DeviceConfig,
+        device_info: Option<DeviceInfo>,
     },
 }
